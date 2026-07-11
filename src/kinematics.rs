b7602879420db37082e1a55ced02c9e54dbab989
RobJMal <robjmal@gmail.com>
2026-07-12 00:23:08 +0200

    feat: updated return type for compute_fk and added check on angles and joints

--- src/kinematics.rs
+++ src/kinematics.rs
@@ -1,7 +1,18 @@
-use crate::types::{RobotModel, Transform};
+use std::{collections::HashMap};
+
+use crate::types::{Link, RobotModel, Transform};
 
 impl RobotModel {
-    pub fn compute_fk(&self, angles: &[f64]) {
+    pub fn compute_fk(&self, angles: &[f64]) -> Result<HashMap<Link, Transform>, Box<dyn std::error::Error>> {
         /* Computes forward kinematics of a model */
+        if angles.len() != self.joints.len() {
+            return Err(format!(
+                "expected {} angles, got {}",
+                self.joints.len(),
+                angles.len()
+            ).into());
+        }
+
+        Ok(HashMap::new())
     }
 }
