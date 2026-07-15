ce9dcc8300066a488fd19f3d076bfd3cc712d55e
Robert Jomar Malate <robjmal@gmail.com>
2026-07-15 13:44:43 +0200

    clean: adding comments in main to describe functionality in main loop

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use taligalaw::load_urdf;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let robot_model = load_urdf("assets/simple_robot.urdf")?;
 
+    // Information about the robot
     println!("robot name: {:?}", robot_model.name);
     println!("number of links: {:?}", robot_model.links.len());
     println!("number of joints: {:?}", robot_model.joints.len());
@@ -15,7 +16,10 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Joint: {:?}", joint.name);
     }
 
+    // Test commands
     let joint_cmds = [0.0, 0.0];
+    
+    // Demo with taligalaw compute_fk
     match robot_model.compute_fk(&joint_cmds) {
         Ok(links) => for link in robot_model.links.iter() {
             println!("{:?}, {:?}", link, links[link])
